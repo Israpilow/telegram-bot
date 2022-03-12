bc36a6c7984da28eb6f4ed66308260fd4ea28d0a
Your Name01~ <ubayd200@gmail.com>
2022-03-12 19:46:29 +0300

    f

--- scenes/animals.js
+++ scenes/animals.js
@@ -26,7 +26,7 @@ nameStep.action('appliances_btn', async ctx => {
     try {
         await ctx.answerCbQuery()
         ctx.wizard.state.data.name = ctx.message;
-        await ctx.replyWithHTML('<b>ШАБЛОН ДЛЯ ЗАПОЛНЕНИЯ ТРАНСПОРТА</b>\n\n<i>1. Название объявления\n2. Состояние(новое|б/у)\n3. Описание объявления\n4. Цена\n5. Адрес\n6. Контакты</i>');
+        await ctx.replyWithHTML('<b>ШАБЛОН ДЛЯ ЗАПОЛНЕНИЯ ЖИВОТНЫЕ</b>\n\n<i>1. Название объявления\n2. Описание объявления\n4. Цена\n5. Адрес\n6. Контакты</i>');
         return ctx.wizard.next()
     } catch (e) {
         console.log(e)
@@ -34,22 +34,22 @@ nameStep.action('appliances_btn', async ctx => {
 })
 
 
-const checkPhotoStep = new Composer()
-checkPhotoStep.on('text', async ctx => {
-    try {
-        ctx.wizard.state.data.checkPhoto = ctx.message.text;
-        await ctx.replyWithHTML(`Отправьте скриншот <b>чека</b>`);
-        return ctx.wizard.next()
-    } catch (e) {
-        console.log(e)
-    }
-})
+// const checkPhotoStep = new Composer()
+// checkPhotoStep.on('text', async ctx => {
+//     try {
+//         ctx.wizard.state.data.checkPhoto = ctx.message.text;
+//         await ctx.replyWithHTML(`Отправьте скриншот <b>чека</b>`);
+//         return ctx.wizard.next()
+//     } catch (e) {
+//         console.log(e)
+//     }
+// })
 
 const photoStep = new Composer()
-photoStep.on('photo', async ctx => {
+photoStep.on('text', async ctx => {
     try {
         
-        ctx.wizard.state.data.photo = ctx.message;
+        ctx.wizard.state.data.photo = ctx.message.text;
         const wizardData = ctx.wizard.state.data;
         await ctx.replyWithHTML(`Отправьте <b>фотографии</b> в одном сообщении\n<i>На данный момент не более 1</i>`);
         return ctx.wizard.next()
@@ -58,15 +58,15 @@ photoStep.on('photo', async ctx => {
     }
 })
 
-photoStep.on('document', async ctx => {
-    try {
-        ctx.wizard.state.data.photo = ctx.message;
-        await ctx.replyWithHTML(`Отправьте <b>фотографии</b> в одном сообщении\n<i>На данный момент не более 1</i>`);
-        return ctx.wizard.next()
-    } catch (e) {
-        console.log(e)
-    }
-})
+// photoStep.on('document', async ctx => {
+//     try {
+//         ctx.wizard.state.data.photo = ctx.message;
+//         await ctx.replyWithHTML(`Отправьте <b>фотографии</b> в одном сообщении\n<i>На данный момент не более 1</i>`);
+//         return ctx.wizard.next()
+//     } catch (e) {
+//         console.log(e)
+//     }
+// })
 
 const conditionStep = new Composer()
 conditionStep.on('photo', async ctx => {
@@ -74,16 +74,14 @@ conditionStep.on('photo', async ctx => {
         ctx.wizard.state.data.condition = ctx.message;
         const wizardData = ctx.wizard.state.data;
         await ctx.replyWithHTML(`Ваше <b>объявление</b> успешно отправлена Администратору!`);
-        await ctx.telegram.sendMessage(1954192936, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.checkPhoto}`, {
+        await ctx.telegram.sendMessage(1954192936, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.photo}`, {
             parse_mode: "HTML"
         });
-        await ctx.telegram.sendMessage(974900206, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.checkPhoto}`, {
+        await ctx.telegram.sendMessage(974900206, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.photo}`, {
             parse_mode: "HTML"
         });
-        await ctx.copyMessage(1954192936, wizardData.photo)
         await ctx.copyMessage(1954192936, wizardData.condition)
 
-        await ctx.copyMessage(974900206, wizardData.photo);
         await ctx.copyMessage(974900206, wizardData.condition);
         return ctx.scene.leave();
     } catch (e) {
@@ -95,18 +93,16 @@ conditionStep.on('document', async ctx => {
     try {
         ctx.wizard.state.data.condition = ctx.message;
         const wizardData = ctx.wizard.state.data;
-        await ctx.replyWithHTML(`${wizardData.checkPhoto}`);
+        await ctx.replyWithHTML(`${wizardData.photo}`);
         await ctx.replyWithHTML(`Ваше <b>объявление</b> успешно отправлена Администратору!`);
-        await ctx.telegram.sendMessage(1954192936, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.checkPhoto}`, {
+        await ctx.telegram.sendMessage(1954192936, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.photo}`, {
             parse_mode: "HTML"
         });
-        await ctx.telegram.sendMessage(974900206, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.checkPhoto}`, {
+        await ctx.telegram.sendMessage(974900206, `<b>ЖИВОТНЫЕ</b>\n\n${wizardData.photo}`, {
             parse_mode: "HTML"
         });
-        await ctx.copyMessage(1954192936, wizardData.photo);
         await ctx.copyMessage(1954192936, wizardData.condition);
 
-        await ctx.copyMessage(974900206, wizardData.photo);
         await ctx.copyMessage(974900206, wizardData.condition);
         return ctx.scene.leave();
     } catch (e) {
@@ -114,5 +110,5 @@ conditionStep.on('document', async ctx => {
     }
 })
 
-const animalsScene = new Scenes.WizardScene('animalsWizard', startStep, nameStep, checkPhotoStep, photoStep, conditionStep)
+const animalsScene = new Scenes.WizardScene('animalsWizard', startStep, nameStep, photoStep, conditionStep)
 module.exports = animalsScene
