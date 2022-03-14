a074381b8cb2026be39f8b1efae65ed92f30d73e
John <ubayd200@gmail.com>
2022-03-14 16:07:10 +0300

    Success transport + electronics

--- scenes/transport.js
+++ scenes/transport.js
@@ -67,10 +67,10 @@ doneStep.on('text', async (ctx) => {
     });
     await ctx.telegram.sendMediaGroup(1954192936, photos[ctx.from.id]);
 
-    // await ctx.telegram.sendMessage(974900206, `<b>ТРАНСПОРТ</b>\n\n<b>${wizardData.photo}</b>`, {
-    //     parse_mode: "HTML"
-    // });
-    // await ctx.telegram.sendMediaGroup(974900206, photos[ctx.from.id]);
+    await ctx.telegram.sendMessage(974900206, `<b>ТРАНСПОРТ</b>\n\n<b>${wizardData.photo}</b>`, {
+        parse_mode: "HTML"
+    });
+    await ctx.telegram.sendMediaGroup(974900206, photos[ctx.from.id]);
     console.log(photos[ctx.from.id]);
     photos[ctx.from.id] = [];
     return ctx.scene.leave();
