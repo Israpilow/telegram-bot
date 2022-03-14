1618c80afc884c4f2536a8acd196ceafd5534f5a
John <ubayd200@gmail.com>
2022-03-14 16:04:34 +0300

    Success transport + electronics

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const vacancyScene = require('./scenes/vacancy.js'),
     servicesScene = require('./scenes/services.js'),
     propertyScene = require('./scenes/property.js'),
     studiosScene = require('./scenes/studios.js'),
-    roomsScene = require('./scenes/rooms.js');
+    roomsScene = require('./scenes/rooms.js'),
+    transportScene = require('./scenes/transport.js'),
+    electronicsScene = require('./scenes/electronics.js');
 
 // Bot
 const bot = new Telegraf(process.env.BOT_TOKEN, {
@@ -25,9 +27,15 @@ const stage = new Scenes.Stage(
         propertyScene,
         studiosScene,
         roomsScene,
+        transportScene,
+        electronicsScene
     ]);
 bot.use(session());
 bot.use(stage.middleware());
+
+
+// Транспорт
+// Электроника
 
 // Start
 bot.command('/start', async ctx => {
@@ -46,6 +54,7 @@ bot.command('/start', async ctx => {
 bot.hears('\u{1F4E2}Подать объявление\u{1F4E2}', async ctx => {
     await ctx.reply('Выберите один из вариантов:', Markup.keyboard([
         [Markup.button.callback("Работа", 'work_btn'), Markup.button.callback("Недвижимость", 'property_btn')],
+        [Markup.button.callback("Транспорт", 'transport_btn'), Markup.button.callback("Электроника", 'electronics_btn')],
         [Markup.button.callback("\u{2B05}Назад", 'back_btn')]
     ]).oneTime().resize());
 });
@@ -80,34 +89,52 @@ bot.hears('Посуточно', async ctx => {
     ]).oneTime().resize());
 });
 
+// Транспорт
+bot.hears('Транспорт', async ctx => {
+    await ctx.reply('Выберите один из вариантов:', Markup.keyboard([
+        [Markup.button.callback("Автомобили", 'avto_btn'), Markup.button.callback(`Мотоциклы и мототехника`, 'moto_btn'), Markup.button.callback(`Грузовики и спецтехника`, 'trucks_btn')],
+        [Markup.button.callback("Водный транспорт", 'water_btn'), Markup.button.callback(`Запчасти и аксессуары`, 'accessories_btn')],
+        [Markup.button.callback("\u{2B05}Назад", 'back_btn')]
+    ]).oneTime().resize());
+});
+
+// Электроника
+bot.hears('Электроника', async ctx => {
+    await ctx.reply('Выберите один из вариантов:', Markup.keyboard([
+        [Markup.button.callback("Телефоны", 'phone_btn'), Markup.button.callback(`Аудио и видео`, 'audio_video_btn'), Markup.button.callback(`Товары для компьютера`, 'goods_btn')],
+        [Markup.button.callback("Игры, приставки и программы", 'water_btn'), Markup.button.callback(`Ноутбуки`, 'laptop_btn'), Markup.button.callback(`Настольные компьютеры`, 'computers_btn')],
+        [Markup.button.callback("Фототехника", 'photo_btn'), Markup.button.callback(`Планшеты и электронные книги`, 'laptop_btn'), Markup.button.callback(`Оргетехника и расходники`, 'computers_btn')],
+        [Markup.button.callback("\u{2B05}Назад", 'back_btn')]
+    ]).oneTime().resize());
+});
+
+// Работа
+bot.hears('Вакансии', ctx => ctx.scene.enter('vacancyWizard'));
+bot.hears('Резюме', ctx => ctx.scene.enter('resumeWizard'));
+bot.hears('Услуги', ctx => ctx.scene.enter('servicesWizard'));
+
 // Недвижемость
-bot.hears('Студии', ctx => {
-    ctx.scene.enter('studiosWizard');
-});
+bot.hears('Студии', ctx => ctx.scene.enter('studiosWizard'));
+bot.hears('Квартиры', ctx => ctx.scene.enter('propertyWizard'));
+bot.hears('Комнаты', ctx => ctx.scene.enter('roomsWizard'));
 
-bot.hears('Квартиры', ctx => {
-    ctx.scene.enter('propertyWizard');
-});
+//Транспорт
+bot.hears('Автомобили', ctx => ctx.scene.enter('transportWizard'));
+bot.hears('Мотоциклы и мототехника', ctx => ctx.scene.enter('transportWizard'));
+bot.hears('Грузовики и спецтехника', ctx => ctx.scene.enter('transportWizard'));
+bot.hears('Водный транспорт', ctx => ctx.scene.enter('transportWizard'));
+bot.hears('Запчасти и аксессуары', ctx => ctx.scene.enter('transportWizard'));
 
-bot.hears('Комнаты', ctx => {
-    ctx.scene.enter('roomsWizard');
-});
-
-bot.hears('Другое', ctx => {
-    ctx.scene.enter('propertyWizard');
-});
-
-bot.hears('Вакансии', ctx => {
-    ctx.scene.enter('vacancyWizard');
-});
-
-bot.hears('Резюме', ctx => {
-    ctx.scene.enter('resumeWizard');
-});
-
-bot.hears('Услуги', ctx => {
-    ctx.scene.enter('servicesWizard');
-});
+//Транспорт
+bot.hears('Телефоны', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Аудио и видео', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Товары для компьютера', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Игры, приставки и программы', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Ноутбуки', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Настольные компьютеры', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Фототехника', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Планшеты и электронные книги', ctx => ctx.scene.enter('electronicsWizard'));
+bot.hears('Оргетехника и расходники', ctx => ctx.scene.enter('electronicsWizard'));
 
 
 //******************************************КАНАЛ С ОБЪЯВЛЕНИЯМИ**************************************************
