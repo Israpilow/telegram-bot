afb05b0f6d7e74da597734683b4574a9db7218c6
John <ubayd200@gmail.com>
2022-03-22 22:29:30 +0300

    fff

--- scenes/electronics.js
+++ scenes/electronics.js
@@ -26,11 +26,13 @@ const photoStep = new Composer();
 photoStep.on('text', async ctx => {
     try {
         ctx.wizard.state.data.photo = ctx.message.text;
+        await ctx.telegram.sendVideo(ctx.from.id, 'https://r10---sn-n8v7sn7e.googlevideo.com/videoplayback?signature=0E774DF51BE97126C12E8F07970FCD4193EF0A6B.7B90EF75C0BB781E89AA79D8C0AEE0E344E1EEB4&id=o-AMbc9AdT4EzEsQI-9uGL7q7PDjpf2WJX9nFwZ0lFlhbQ&upn=K8KQqQKgpgs&ip=83.234.245.86&pl=24&itag=248&gir=yes&requiressl=yes&source=youtube&lmt=1465554079496878&sparams=clen,dur,expire,gir,id,initcwndbps,ip,ipbits,itag,lmt,mime,mm,mn,ms,mv,pl,requiressl,source,upn&ipbits=0&clen=40460806&dur=121.788&key=cms1&mime=video%2Fwebm&expire=1477509693&cpn=QPMs12hM3tDvx18I&alr=yes&keepalive=yes&ratebypass=yes&c=WEB&cver=1.20161025&redirect_counter=1&cm2rm=sn-ug5onuxaxjvh-gufe7l&cms_redirect=yes&mm=30&mn=sn-n8v7sn7e&ms=nxu&mt=1477487952&mv=m&req_id=9ed811f2b7b8c3fd&')
         photos[ctx.from.id] = [];
         await ctx.reply(`Пришлите фотографии.\nНажмите на кнопку чтобы отправить`, Markup.keyboard([
             [Markup.button.callback('Отправить', 'send')],
             [Markup.button.callback("\u{2B05}Назад", 'back_btn')]
         ]).oneTime().resize());
+        
         return ctx.wizard.next();
     } catch (e) {
         console.log(e);
